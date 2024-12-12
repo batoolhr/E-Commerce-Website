8f86dbbce9837baf8d6a1369b33d6bd09928e5d8
Batool <batoolharah@outlook.com>
2024-12-12 19:18:07 +0200

    Code enhancements

--- src/Components/Products/Categories.jsx
+++ src/Components/Products/Categories.jsx
@@ -1,32 +1,36 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getCategories } from '../../Store/CategoriesSlice';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategories } from "../../Store/CategoriesSlice";
 
-  const Categories = ({HandleSelectCategory}) => {
+const Categories = ({ HandleSelectCategory }) => {
   const Category = useSelector((state) => state.Categories.cat);
-  const dispatch=useDispatch()
-  useEffect(()=>{
-    dispatch(getCategories())
-  },[dispatch])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
 
   return (
-    <div className='w-full max-w-[890px] mx-auto '>
-       <h1 className='text-center text-bt-color font-bold text-2xl my-9'>LATEST PRODUCTS </h1>
-      <div className='flex gap-2 items-center'>
-      <div  onClick={()=>HandleSelectCategory('All')} className="flex hover:cursor-pointer" >
-            <span
-              
-              className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-bt-color bg-[#E5E4E2] hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-            >
-              ALL
-            </span>
-          </div>
+    <div className="w-full max-w-[890px] mx-auto ">
+      <h1 className="text-center text-bt-color font-bold text-2xl my-9">
+        LATEST PRODUCTS{" "}
+      </h1>
+      <div className="flex gap-2 items-center">
+        <div
+          onClick={() => HandleSelectCategory("All")}
+          className="flex hover:cursor-pointer"
+        >
+          <span className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-bt-color bg-[#E5E4E2] hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+            ALL
+          </span>
+        </div>
         {Category.map((item, index) => (
-          <div  onClick={()=>HandleSelectCategory(item)} className="flex  hover:cursor-pointer" key={index} >
-            <span 
-              className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-bt-color bg-[#E5E4E2] hover:bg-indigo-200 md:py-4  md:px-10"
-            >
-              {item}
+          <div
+            onClick={() => HandleSelectCategory(item.title)}
+            className="flex  hover:cursor-pointer"
+            key={index}
+          >
+            <span className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-bt-color bg-[#E5E4E2] hover:bg-indigo-200 md:py-4  md:px-10">
+              {item.title}
             </span>
           </div>
         ))}
--- src/Components/Products/ProductCard.jsx
+++ src/Components/Products/ProductCard.jsx
@@ -8,45 +8,48 @@ const ProductCard = (props) => {
   const HandleAddtoCart = (data) => {
     dispatch(addTolist(data));
   };
- 
-  const navigate=useNavigate()
-  const navToDEtails=(data)=>{
+
+  const navigate = useNavigate();
+  const navToDEtails = (data) => {
     navigate(`/ProductDetails`, { state: { productData: props.data } });
-  }
+  };
 
   return (
-  
-
-     <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-72">
-  <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96 hover:scale-[0.97] cursor-pointer"
-  onClick={()=>navToDEtails()}
-  >
-    <img  src={props.data.image}  alt="card " className="object-contain w-full h-full" />
-  </div>
-  <div className="p-6">
-    <div className="flex items-center justify-between mb-2">
-       <div
-          className="font-bold text-xl mb-2 overflow-hidden h-[3.6em] block font-sans  antialiased  leading-relaxed text-blue-gray-900"
-          title={props.data.title}
-         >
-           {/* {hoverTitle ? props.data.title : props.data.title.slice(0, 40) + "..."} */}
-           {props.data.title}
-         </div>
-      
+    <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-72">
+      <div
+        className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96 hover:scale-[0.97] cursor-pointer"
+        onClick={() => navToDEtails()}
+      >
+        <img
+          src={props.data.image}
+          alt="card "
+          className="object-contain w-full h-full"
+        />
+      </div>
+      <div className="p-6">
+        <div className="flex items-center justify-between mb-2">
+          <div
+            className="font-bold text-xl mb-2 overflow-hidden h-[3.6em] block font-sans  antialiased  leading-relaxed text-blue-gray-900"
+            title={props.data.title}
+          >
+            {/* {hoverTitle ? props.data.title : props.data.title.slice(0, 40) + "..."} */}
+            {props.data.title}
+          </div>
+        </div>
+      </div>
+      <div className="p-6 pt-0 flex items-center justify-between">
+        <button
+          onClick={() => HandleAddtoCart(props.data)}
+          className="bg-[#E5E4E2] py-[10px] px-3 rounded-md text-bt-color font-semibold hover:bg-indigo-200 
+       hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+        >
+          Add to Cart
+        </button>
+        <p className="block font-sans text-base antialiased font-medium leading-relaxed text-bt-color">
+          ${props.data.price}
+        </p>
+      </div>
     </div>
-  </div>
-  <div className="p-6 pt-0 flex items-center justify-between">
-    <button
-      onClick={() => HandleAddtoCart(props.data)}
-      className="bg-[#E5E4E2] py-[10px] px-3 rounded-md text-bt-color font-semibold hover:bg-indigo-200 
-       hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100">
-      Add to Cart
-    </button>
-    <p className="block font-sans text-base antialiased font-medium leading-relaxed text-bt-color">
-        ${props.data.price}
-      </p>
-  </div>
-</div>
   );
 };
 
